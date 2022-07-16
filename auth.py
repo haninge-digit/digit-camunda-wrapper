45d64f06cbc2ba955cf606e8a56165bcb54af478
hakan-persson <hakan.persson@haninge.se>
2022-07-16 09:36:00 +0200

    Cleanup

--- auth.py
+++ auth.py
@@ -4,36 +4,22 @@ import sanic
 import jwt
 
 
-# def check_token(request):
-#     if not request.token:
-#         return False
-
-#     try:
-#         jwt.decode(
-#             request.token, request.app.config.secret, algorithms=["HS256"]
-#         )
-#     except jwt.exceptions.InvalidTokenError:
-#         return False
-#     else:
-#         return True
-
 def token_is_valid(request) -> bool:
-    if request.app.ctx.disable_auth:    # Authentication is disabled
-        return True
-    if not request.token:
-        logging.error("Missing JWT token in request")
-        return False
-    try:
-        jwt.decode(request.token, request.app.ctx.jwt_secret, options={'require':["exp"]}, algorithms="HS256")
-    except jwt.InvalidSignatureError:
-        logging.error("JWT token has an invalid signature")
-        return False        # Missing JWT token in request
-    except jwt.ExpiredSignatureError:
-        logging.error("JWT token has expired")
-        return False
-    except jwt.MissingRequiredClaimError:
-        logging.error("Missing 'exp' claim in JWT token")
-        return False
+    if not request.app.ctx.disable_auth:    # Authentication is enabled
+        if not request.token:
+            logging.error("Missing JWT token in request")
+            return False
+        try:
+            jwt.decode(request.token, request.app.ctx.jwt_secret, options={'require':["exp"]}, algorithms="HS256")
+        except jwt.InvalidSignatureError:
+            logging.error("JWT token has an invalid signature")
+            return False
+        except jwt.ExpiredSignatureError:
+            logging.error("JWT token has expired")
+            return False
+        except jwt.MissingRequiredClaimError:
+            logging.error("Missing 'exp' claim in JWT token")
+            return False
 
     return True     # All good!
 
