c45476d00250335719fc962d6a0618a9ab14f908
Håkan Persson <hakan.persson@haninge.se>
2022-08-11 10:49:41 +0200

    Merge lab into main (#4)
    * Run on all branches
    
    * Changed API from /process to /workflow
    
    * "${{ env.IMAGE_NAME }}" on line 26 not ok?
    
    * Set JWT_SECRET to None if it doesn't exist
    
    * Moved variables to far up...
    
    * Pass along http method in worker call
    
    * Just return what worker returns
    
    * Changed ZEEBE_ADDRESS default value
    
    * Error reponse possibilities from worker
    
    * Delete query_args from worker response
    
    * Functional version of the tasks API
    
    * Moved some files
    
    * Renamed /tasks API to /task
    
    * A new README

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# camunda-python-wrapper
+# digit-camunda-wrapper
 #
-# This is a convenience API that wraps Camunda process API
-# It implements a GET and a POST method where the GET method waits for Camunda to get to a manual task, where the result is picked up and returned
+# This is a REST and convinience API that wraps Camunda gRPC API
 # 
 
 import os
@@ -28,16 +27,16 @@ from auth import protected
 """ 
 Environment
 """
-ZEEBE_ADDRESS = os.getenv('ZEEBE_ADDRESS',"localhost:26500")            # Zeebe adress and port
-DEBUG_MODE = os.getenv('DEBUG','false') == "true"                       # Enable global DEBUG logging
-DEV_MODE = os.getenv('DEV_MODE','false') == "true"                      # Sanic develpoment mode
+ZEEBE_ADDRESS = os.getenv('ZEEBE_ADDRESS',"camunda8-zeebe-gateway:26500")   # Zeebe address and port
+DEBUG_MODE = os.getenv('DEBUG','false') == "true"                           # Enable global DEBUG logging
+DEV_MODE = os.getenv('DEV_MODE','false') == "true"                          # Sanic develpoment mode
 
-JWT_SECRET = os.getenv('JWT_SECRET')                                    # Secret (!!!) for JWT generation and verification
-DISABLE_AUTH = os.getenv('DISABLE_AUTH','false') == "true"              # Disable API authentication for testing purposes
-DISABLE_TASK_API = os.getenv('DISABLE_TASK_API','false') == "true"      # Disable wrapper task API for testing purposes
+JWT_SECRET = os.getenv('JWT_SECRET',None)                                   # Secret (!!!) for JWT generation and verification
+DISABLE_AUTH = os.getenv('DISABLE_AUTH','false') == "true"                  # Disable API authentication for testing purposes
+DISABLE_TASK_API = os.getenv('DISABLE_TASK_API','false') == "true"          # Disable wrapper task API for testing purposes
 
-MAX_TIME = 60                                                           # Max time in seconds to wait for a GET to return result
-LOGFORMAT = "%(asctime)s %(funcName)-10s [%(levelname)s] %(message)s"   # Log format
+MAX_TIME = 60                                                               # Max time in seconds to wait for a GET to return result
+LOGFORMAT = "%(asctime)s %(funcName)-10s [%(levelname)s] %(message)s"       # Log format
 
 
 """ 
@@ -58,7 +57,7 @@ def startup(app, loop):
     app.ctx.channel = grpc.aio.insecure_channel(ZEEBE_ADDRESS)
     app.ctx.stub = gateway_pb2_grpc.GatewayStub(app.ctx.channel)
     if not DISABLE_TASK_API:
-        app.ctx.collect_task = asyncio.create_task(collect_task(app.ctx))    # Start collect task
+        app.ctx.collect_tasks = asyncio.create_task(collect_tasks(app.ctx))    # Start collect task
 
 @app.after_server_stop
 def shutdown(app, loop):
@@ -67,45 +66,19 @@ def shutdown(app, loop):
     # app.ctx.channel.close()   # Can't do close() here! Can be skipped?
 
 
-"""
-Asynchronous task that periodically collects active tasks from Camunda
-"""
-async def collect_task(ctx):
-    logging.info("Collect Task started!")
-    ctx.active_tasks = []
-    topic = "io.camunda.zeebe:userTask"     # Worker topic for all BPMN user tasks in Zeebe
-    locktime = 1                            # Don't know if this works?
-    worker_id = str(uuid.uuid4().time_low)  # Random worker ID
-    ajr = ActivateJobsRequest(type=topic,worker=worker_id,timeout=locktime,
-                              maxJobsToActivate=10000,requestTimeout=-1)    # Get user tasks request
-    while(ctx.running):
-        async for response in ctx.stub.ActivateJobs(ajr):   # Get all active user tasks
-            active_tasks = []
-            logging.debug(f"Found {len(response.jobs)} active user tasks")
-            for job in response.jobs:   # Loop through all returned user tasks
-                task = {}
-                task['key'] = job.key
-                task['bpmnProcessId'] = job.bpmnProcessId
-                task['elementId'] = job.elementId
-                task['customHeaders'] = json.loads(job.customHeaders)
-                task['variables'] = json.loads(job.variables)
-                active_tasks.append(task)   # Append it to the active_tasks list
-
-        ctx.active_tasks = active_tasks     # Save it for global use
-        await asyncio.sleep(30)      # Collect tasks every 30 seconds
-
-    logging.info("Collect Task stopped!")
-
-
 """ 
 Worker API
+This is a GET which calls a Camunda worker and returns the result from that worker.
+All results are in JSON format.
+Testing with PATCH method as well
 """
-@app.route("/worker/<worker_name:str>", methods=['GET'])
+@app.route("/worker/<worker_name:str>", methods=['GET', 'PATCH'])
 @protected      # API requires a valid JWT token
-async def start_worker(request, worker_name: str):
+async def start_worker(request, worker_name:str):
     stub = request.app.ctx.stub
-    query_args = {q[0]:q[1] for q in request.get_query_args(keep_blank_values=True)}     # Grab all query_args
 
+    query_args = {q[0]:q[1] for q in request.get_query_args(keep_blank_values=True)}     # Grab all query_args
+    query_args['HTTP_METHOD'] = request.method  # Pass request method
     userid = query_args.get('userid',"")    # Just for logging
     logg_id = str(uuid.uuid4().time_low)    # Just for logging
 
@@ -120,53 +93,53 @@ async def start_worker(request, worker_name: str):
         return handle_grpc_errors(grpc_error, worker_name)
 
     res = json.loads(response.variables)
+    if 'DIGIT_ERROR' in res:
+        return sanic.text(res['DIGIT_ERROR'], status=400)  # Bad request
+
     for k in query_args:
-        res.pop(k,None)              # Delete query_args if still around (Should be done in the worker?)
-    if 'RESOBJ' in res:             # Return values are a complete json object (should be in DB?)
-        res = json.loads(res['RESOBJ']['value'])
+        res.pop(k,None)              # Delete query_args from response
 
     return sanic.json(res)
 
 
 """ 
-Process API
+Workflow API
+This is a POST which starts a workflow in Camunda.
+A reference to the started workflow is returned in JSON format.
 """
-@app.route("/process/<process_name:str>", methods=['GET', 'POST'])
+@app.route("/workflow/<workflow_name:str>", methods=['POST'])
 @protected      # API requires a valid JWT token
-async def start_process(request, process_name: str):
-    if request.method == "GET":
-        return await start_worker(request,process_name)  # This is actually an worker call?
-
+async def start_workflow(request, workflow_name:str):
     stub = request.app.ctx.stub
 
     query_args = {q[0]:q[1] for q in request.get_query_args(keep_blank_values=True)}     # Grab all query_args
     json_body = {'JSON_BODY':json.dumps(request.json)}         # And the JSON body
-    local_args = {'HTTP_METHOD':request.method, 'PROCESS_NAME':process_name}  # Pass request method and called process
+    local_args = {'HTTP_METHOD':request.method, 'workflow_name':workflow_name}  # Pass request method and called process
     params = query_args | json_body | local_args
-
     userid = query_args.get('userid',"")    # Just for logging
     logg_id = str(uuid.uuid4().time_low)    # Just for logging
 
     try:
-        logging.info(f"Process start.   Loggid={logg_id};  Process={process_name};  userID={userid}")
+        logging.info(f"Workflow start.   Loggid={logg_id};  Process={workflow_name};  userID={userid}")
         response = await stub.CreateProcessInstance(
-            CreateProcessInstanceRequest(bpmnProcessId=process_name, version=-1, variables=json.dumps(params)))
-        logging.info(f"Process started. Loggid={logg_id}  Version={response.version}  Instance={response.processInstanceKey}")
+            CreateProcessInstanceRequest(bpmnProcessId=workflow_name, version=-1, variables=json.dumps(params)))
+        logging.info(f"Workflow started. Loggid={logg_id}  Version={response.version}  Instance={response.processInstanceKey}")
     except grpc.aio.AioRpcError as grpc_error:
-        return handle_grpc_errors(grpc_error, process_name)
+        return handle_grpc_errors(grpc_error, workflow_name)
 
     return sanic.json({'processID':response.processInstanceKey})
     
 
 """ 
 Epi forms API
+Special API that is "forms aware". Always a POST that starts a process in Camunda.
+A reference to the started process is returned in JSON format.
 """
-@app.route("/form/process/<process_name:str>", methods=['POST'])
+@app.route("/form/<form_process:str>", methods=['POST'])
 @protected      # Requires a valid JWT token
-async def handler(request, process_name: str):
-    logging.debug(f"Form post to process={process_name}")
-
+async def handle_form(request, form_process:str):
     stub = request.app.ctx.stub
+    logg_id = str(uuid.uuid4().time_low)    # Just for logging
 
     if request.content_type == "application/json":
         logging.debug(f"Post JSON form with keys={list(request.json)}")
@@ -176,15 +149,14 @@ async def handler(request, process_name: str):
         params = {k:{"value":v[0]} for k,v in request.form.items()}        # POST has a form body with key/value pairs and with only string values
 
     userid = params.get('userid',"")    # It won't be here. Need to grab it from the form instead
-    logg_id = str(uuid.uuid4().time_low)    # Just for logging
 
     try:
-        logging.info(f"Process start.   Loggid={logg_id};  Process={process_name};  userID={userid}")
+        logging.info(f"Process start.   Loggid={logg_id};  Process={form_process};  userID={userid}")
         response = await stub.CreateProcessInstance(
-            CreateProcessInstanceRequest(bpmnProcessId=process_name, version=-1, variables=json.dumps(params)))
+            CreateProcessInstanceRequest(bpmnProcessId=form_process, version=-1, variables=json.dumps(params)))
         logging.info(f"Process started. Loggid={logg_id}  Version={response.version}  Instance={response.processInstanceKey}")
     except grpc.aio.AioRpcError as grpc_error:
-        return handle_grpc_errors(grpc_error, process_name)
+        return handle_grpc_errors(grpc_error, form_process)
 
     return sanic.text("POSTED")
 
@@ -192,21 +164,61 @@ async def handler(request, process_name: str):
 """
 Task API
 """
-@app.route("/tasks/<jobKey:strorempty>", methods=['GET', 'POST'])
+@app.route("/task/<task_key:strorempty>", methods=['GET', 'POST'])
 @protected      # Requires a valid JWT token
-async def handler(request):
-    # GET method returns current acrive user tasks
-    # query args can be bpmnProcessId, elementId and/or assignedTo
-    # No query args returns all user tasks
+async def handler(request, task_key:str):
+    if DISABLE_TASK_API:
+        return sanic.text("Task API is currently not available", status=501)
+
+    query_args = {q[0]:q[1] for q in request.get_query_args(keep_blank_values=True)}     # Grab all query_args
+
+    userid = query_args.get('userid',"")
+    if userid == "":
+        return sanic.text("Anonymous task requests are not allowed", status=401)
+    
     if request.method == "GET":
-        query_args = {q[0]:q[1] for q in request.get_query_args(keep_blank_values=True)}     # Grab all query_args
-        res = request.app.ctx.active_tasks
-        # TODO: Filter out tasks
-        return sanic.json(res)
+        if task_key == "all" or task_key == "":             # A list of all tasks for a user is requested
+            task_id = query_args.get('task_id',"")          # Filter on specifik tasks
+            workflow_id = query_args.get('workflow_id',"")  # and/or specific workflows
+
+            found_tasks = []
+            for key,task in request.app.ctx.active_tasks.items():
+                # task = request.app.ctx.active_tasks[key]
+                if task['assignee'] == userid and (task_id == "" or task_id == task['task_id']) and (workflow_id == "" or workflow_id == task['workflow_id']):
+                    found_tasks.append({'task_key':key, 'task_id':task['task_id'], 'workflow_id':task['workflow_id']})     # Add task that matches
+
+            return sanic.json({'tasks':found_tasks})    # Return the list. Can be empty.
+
+        else:
+            if task_key not in request.app.ctx.active_tasks:
+                return sanic.text(f"Task with key {task_key} not found!", status=404)
+            task = request.app.ctx.active_tasks[task_key]
+            if task_key not in request.app.ctx.active_tasks[task_key]['assignee'] != userid:
+                return sanic.text(f"User {userid} can't retrieve tasks assigned to {request.app.ctx.active_tasks[task_key]['assignee']}", status=403)
+            # TODO: Filter out and complete with relevant information to return
+            return sanic.json(task)                     # Return information about a specific task.
 
     # POST method completes the requested task with potential updated variables
-    # await stub.CompleteJob(gateway_pb2.CompleteJobRequest(jobKey=job.key, variables=json.dumps(payload)))
-    return sanic.text("OK")
+    if task_key == "":
+        return sanic.text(f"Post task must have a valid key in the URL!", status=400)
+    if task_key not in request.app.ctx.active_tasks:
+        return sanic.text(f"Task with key {task_key} not found!", status=404)
+    if task_key not in request.app.ctx.active_tasks[task_key]['assignee'] != userid:
+        return sanic.text(f"User {userid} can't complete tasks assigned to {request.app.ctx.active_tasks[task_key]['assignee']}", status=403)
+
+    if request.content_type == "application/json":
+        payload = request.json  # Some new variables to add to flow
+    else:
+        payload = {}            # Nothing to add
+    try:
+        cjr = CompleteJobRequest(jobKey=int(task_key), variables=json.dumps(payload))   # Complete task with possibly added variables
+        await request.app.ctx.stub.CompleteJob(cjr)     # Do it!
+        if task_key in request.app.ctx.active_tasks:
+            del request.app.ctx.active_tasks[task_key]      # Delete it from active task list. Would have been removed at the next collect_tasks loop.
+    except grpc.aio.AioRpcError as grpc_error:
+        return handle_grpc_errors(grpc_error, "task completion")
+
+    return sanic.text("Completed")
 
 
 """
@@ -238,6 +250,56 @@ async def handler(request):
     t.append(f"Partitions count = {topology.partitionsCount}")
     t.append(f"Replication factor = {topology.replicationFactor}")
     return sanic.text("\n".join(t)+"\n")
+
+
+""" 
+Old process API
+Will be removed when v1.0.0 is released!
+"""
+@app.route("/process/<process_name:str>", methods=['GET', 'POST'])
+@protected      # API requires a valid JWT token
+async def start_process(request, process_name: str):
+    if request.method == "GET":
+        return await start_worker(request,process_name)  # This is now a worker call
+    if request.method == "POST":
+        return await start_workflow(request,process_name)  # This is now a workflow call
+
+
+"""
+Asynchronous task that periodically collects active tasks from Camunda
+"""
+async def collect_tasks(ctx):
+    worker_id = str(uuid.uuid4().time_low)  # Random worker ID
+    logging.info(f"Started to collect user tasks with worker {worker_id}")
+
+    ctx.active_tasks = {}                   # Holds all active user tasks. Task id (job.key) is the key
+    topic = "io.camunda.zeebe:userTask"     # Worker topic for all BPMN user tasks in Zeebe
+    locktime = 1*60*1000                    # A too long time will create a delay on restarts (when task status is lost)
+    max_jobcnt = 10000                      # Can't be too many?
+    max_poll_time = 2*60*1000               # Probaly "lagom". If less than locktime, poll will return after lock expires
+    ajr = ActivateJobsRequest(type=topic,worker=worker_id,
+                              timeout=locktime,
+                              maxJobsToActivate=max_jobcnt,
+                              requestTimeout=max_poll_time)    # Get user tasks request
+
+    while(ctx.running):
+        logging.debug(f"Looking for new tasks")
+        active_tasks = {}
+        async for response in ctx.stub.ActivateJobs(ajr):   # Get all active user tasks
+            logging.debug(f"Found {len(response.jobs)} active user tasks")
+            for job in response.jobs:   # Loop through all returned user tasks
+                task = {}
+                task['workflow_id'] = job.bpmnProcessId
+                task['task_id'] = job.elementId
+                task['task_variables'] = json.loads(job.customHeaders)
+                task['assignee'] = task['task_variables']['io.camunda.zeebe:assignee']
+                task['workflow_variables'] = json.loads(job.variables)
+                active_tasks[str(job.key)] = task   # Add it to the active_tasks list. Task key (a string) is the key
+                logging.debug(f"Task {job.key} is assigned to {task['assignee']}")
+
+        ctx.active_tasks = active_tasks     # Save it for global use
+
+    logging.info("collect_tasks stopped!")
 
 
 """
@@ -295,6 +357,9 @@ def main():
 
     if DISABLE_AUTH:
         logging.info("API authentication is disabled!")
+    if DISABLE_TASK_API:
+        logging.info("Task API is disabled!")
+
 
     app.run(host="0.0.0.0", port=8000, access_log=DEBUG_MODE, motd=DEBUG_MODE, dev=DEV_MODE)      # Run a single worker on port 8000
 
